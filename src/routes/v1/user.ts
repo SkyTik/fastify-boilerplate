369023246eb2859082355e5737fdc2b27c32acad
vunguyen <hoangvu271297@gmail.com>
2025-11-27 17:42:34 +0700

    fix: rename massRoutes to userRoutes for clarity

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -2,8 +2,8 @@ import { FastifyInstance, FastifyPluginAsync } from "fastify";
 
 import { getUser } from "../../controllers/userController.js";
 
-const massRoutes: FastifyPluginAsync = async (fastify: FastifyInstance) => {
+const userRoutes: FastifyPluginAsync = async (fastify: FastifyInstance) => {
   fastify.get("/:userId", getUser);
 };
 
-export default massRoutes;
+export default userRoutes;
